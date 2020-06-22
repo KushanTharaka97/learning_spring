45d62003217a0ff92f101cd45cd38f3954be6930
Kushan Tharaka <tharakakushan@gmail.com>
2020-06-23 04:48:12 +0530

    Initial Comment in learning_spring

---
+++ src/main/java/com/kushanTharaka/selfLearnSpring/learningspring/LearningSpringApplication.java
@@ -0,0 +1,13 @@
+package com.kushanTharaka.selfLearnSpring.learningspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringApplication.class, args);
+	}
+
+}
