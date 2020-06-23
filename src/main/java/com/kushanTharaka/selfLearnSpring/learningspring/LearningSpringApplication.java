a251de849750dec2e6eb29622dfe0c7902825b15
Kushan Tharaka <tharakakushan@gmail.com>
2020-06-23 06:04:11 +0530

    Main Class Mapped with the data and annotations used

--- src/main/java/com/kushanTharaka/selfLearnSpring/learningspring/LearningSpringApplication.java
+++ src/main/java/com/kushanTharaka/selfLearnSpring/learningspring/LearningSpringApplication.java
@@ -1,13 +1,30 @@
 package com.kushanTharaka.selfLearnSpring.learningspring;
 
+import com.kushanTharaka.selfLearnSpring.learningspring.data.entity.Room;
+import com.kushanTharaka.selfLearnSpring.learningspring.data.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class LearningSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearningSpringApplication.class, args);
+	}
+
+	@RestController
+	@RequestMapping("/rooms")
+	public class RoomController{
+		@Autowired
+		private RoomRepository roomRepository;
+
+		public Iterable<Room> getRooms(){
+				return this.roomRepository.findAll();
+
+		}
 	}
 
 }
