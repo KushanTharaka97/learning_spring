e8363c50d9b0bdc21812dfe0d239525569043f8b
Kushan Tharaka <tharakakushan@gmail.com>
2020-06-23 06:15:43 +0530

    BUG_FIXED:@GetMapping annotation missed on the main Class Iterable. BUG: not data pass through the application. Main Class Mapped with the data and annotations used

--- src/main/java/com/kushanTharaka/selfLearnSpring/learningspring/LearningSpringApplication.java
+++ src/main/java/com/kushanTharaka/selfLearnSpring/learningspring/LearningSpringApplication.java
@@ -5,6 +5,7 @@ import com.kushanTharaka.selfLearnSpring.learningspring.data.repository.RoomRepo
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,7 @@ public class LearningSpringApplication {
 		@Autowired
 		private RoomRepository roomRepository;
 
+		@GetMapping
 		public Iterable<Room> getRooms(){
 				return this.roomRepository.findAll();
 
