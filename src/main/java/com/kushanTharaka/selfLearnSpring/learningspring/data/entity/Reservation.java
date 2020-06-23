fa4c118bc970762e4526e731fad8ea77ae7ff36b
Kushan Tharaka <tharakakushan@gmail.com>
2020-06-23 07:31:22 +0530

    CREATED reservation Entity

--- src/main/java/com/kushanTharaka/selfLearnSpring/learningspring/data/entity/Reservation.java
+++ src/main/java/com/kushanTharaka/selfLearnSpring/learningspring/data/entity/Reservation.java
@@ -1,4 +1,22 @@
 package com.kushanTharaka.selfLearnSpring.learningspring.data.entity;
+import javax.persistence.*;
+import java.util.Date;
 
+@Entity
+@Table(name = "RESERVATION")
 public class Reservation {
+
+    @Id
+    @Column(name = "RESERVATION_ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long reservationId;
+
+    @Column(name = "ROOM_ID")
+    private Long roomId;
+
+    @Column(name = "GUEST_ID")
+    private Long guestId;
+
+    @Column(name = "RES_DATE")
+    private Date resDate;
 }
